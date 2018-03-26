bedfe1c23e10e40da4dac049d442508671aabfa6
Grahamjg <ggent@erwin.com>
2018-03-26 19:13:30 +0100

    Just want to keep thes changes although most are commented
    Trying to use jQuery in this application

--- todo.js
+++ todo.js
@@ -1,28 +1,56 @@
 var express = require('express');
 var cookiesession = require('cookie-session');
 var bodyparser = require('body-parser');
-//var todoItem = require('./todoitem');
 
 var todo = express();
-var server = require('http').createServer(todo);
+
+//var jsdom = require("jsdom");
+//var window = jsdom.window;
+// window.$ = require('jquery')(window);
+//require("jsdom").env("", function(err, window) {
+//    if (err) {
+//        console.error(err);
+//      return;
+//   }
+ //
+ //   var $ = require("jquery")(window);
+//});
+
+//var $ = require('jquery');
+//var $ = require('jquery')(require("jsdom").parentWindow);
+//var jsdom = require("jsdom"); 
+//$ = require("jquery"); 
+//window.$ = require('jquery')(window);
+//var jsdom = require('jsdom')
+//  , myWindow = jsdom.createWindow
+  
+//  ;
+//var aWindow = $.createWindow;
+//var $ = require('jQuery').createWindow
+
+var http = require('http');
+var server = http.createServer(todo);
 var io = require('socket.io')(server);
-var urlencodedParser = bodyparser.urlencoded({ extended: false });
+var urlencodedParser = bodyparser.urlencoded({ extended: true });
+var mySocket;
 
 var tdo = function todoitem(description, completiondate, status){
     this.description = description;
     this.completiondate = completiondate;
     this.status = status;
-    //this.desc = function() {
-    //    return this.description;
-    //};
-    
+};
+
+var options = {
+    host: 'localhost',
+    port: 8080,
+    path: '/todo'
 };
 
 tdo.prototype.desc = function(){
     return this.description;
 };
 
-
+var html = '';
 todo.use(cookiesession({
     name : 'todo',
     keys : ['todosecret'],
@@ -64,12 +92,25 @@ create an empty array before continuing */
     res.redirect('/todo');
 })
 
+.get('/todo/edit/:id',  function(req, res) {
 
+    if (req.params.id != '') {
+        var item = req.session.todolist.slice(req.params.id, 1);
+        //$("#newtodo").val("hello");
+        //mySocket.emit('edit', item);
+        mySocket.emit('messages', 'Hello from server')
+ 
+    }
+ 
+})
 
 io.sockets.on('connection', function(socket) {
     console.log('A client is connected');
+    //session.socket = socket;
+    mySocket = socket;
     socket.on('joined', function (data) {
         console.log(data);
+        //socket.emit('messages', 'Hello from server')
     });
     
 });
@@ -78,9 +119,6 @@ todo.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.status(404).send('Page cannot be found!');
 });
-
-
-
 
 //todo.listen(8080);
 server.listen(8080);
